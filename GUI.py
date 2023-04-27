7fdba83e548539ebf5f8fc8d468545945a69245a
marawanFawzy <marawan20191700616@cis.asu.edu.eg>
2023-04-27 18:00:14 +0200

    final

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import networkx as nx
-from networkx.algorithms import community
+from networkx.algorithms import community, cuts
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (
@@ -10,82 +10,113 @@ from pyvis import network as net
 from IPython.core.display import HTML
 from IPython.display import display
 import itertools
+from sklearn.metrics.cluster import normalized_mutual_info_score
 
-# Read nodes from the CSV file
-with open('metadata_primaryschool_Nodes.csv', 'r', encoding='utf-8-sig') as f:
-    reader = csv.reader(f)
-    next(reader)
-    nodes = [(int(row[0]), {'Class': row[1], 'Gender': row[2]})
-             for row in reader]
-
-# Read edges from the CSV file
-with open('primaryschool_Edges.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader)
-    edges = [(int(row[0]), int(row[1])) for row in reader]
-
-# to get nodes and edges fom csv
-# G = nx.Graph()
-# G.add_nodes_from(nodes)
-# G.add_edges_from(edges)
-
-# create html page with size 500 * 70% with name Zachary’s Karate Club graph and menu and filter
-g4 = net.Network(height='500px', width='70%',
-                 select_menu=True,
-                 filter_menu=True, neighborhood_highlight=True)
-
-# dataset karate_club_graph
 G = nx.karate_club_graph()
 
-# Run the Girvan Newman algorithm
-comp = community.girvan_newman(G)
-# girvan_newman = community.girvan_newman(G)
+#directed graph example
+#G = nx.gn_graph(10, kernel=lambda x: x ** 1.5)
+G = G.to_undirected()
+# create html page with size 500 * 70% with name Zachary’s Karate Club graph and menu and filter
+graph = net.Network(height='500px', width='70%',
+                    select_menu=True,
+                    filter_menu=True, neighborhood_highlight=True)
 
-k = 3
-# number of communitis = k+1
-for communities in itertools.islice(comp, k):
+# dataset karate_club_graph
+print("code start")
+
+# Run the Girvan Newman algorithm
+
+k = len(G.nodes)
+
+mods = dict()
+comms = dict()
+comms_it = 2
+
+for communities in itertools.islice(community.girvan_newman(G), k):
     comm = tuple(sorted(c) for c in communities)
-print(comm)
+    mods[comms_it] = community.modularity(G, comm)
+    comms[comms_it] = comm
+    comms_it += 1
+
+best_partion = max(mods, key=mods.get)
+number = best_partion
+mod = mods[best_partion]
+comm = comms[best_partion]
+
 i = 0
 communities = dict()
 for c in comm:
     for j in c:
         communities[j] = i+1
     i = i + 1
-# scale to change the sizes
-scale = 5  # Scaling the size of the nodes by 10*degree
-# tp get degree for each node to dict and * by scale
-d = dict(G.degree)
 
-# Updating dict
-d.update((x, scale*y) for x, y in d.items())
-# Setting up size attribute to filter on size
-nx.set_node_attributes(G, d, 'size')
 
 betweenness_centrality = nx.betweenness_centrality(G, None, False, None)
 closeness_centrality = nx.closeness_centrality(G)
 eigenvector_centrality = nx.eigenvector_centrality(G)
+print(eigenvector_centrality)
 pagerank = nx.pagerank(G)
+page_rank_value = max(pagerank, key=pagerank.get)
 
+d = dict(G.degree)
+max_color = max(d, key=d.get)
+max_color = d[max_color]
+
+nx.set_node_attributes(G, d, 'degree')
+d.update((x, 5*y) for x, y in d.items())
+nx.set_node_attributes(G, d, 'size')
 nx.set_node_attributes(G, betweenness_centrality, 'betweenness_centrality')
 nx.set_node_attributes(G, closeness_centrality, 'closeness_centrality')
 nx.set_node_attributes(G, eigenvector_centrality, 'eigenvector_centrality')
 nx.set_node_attributes(G, communities, 'community')
 nx.set_node_attributes(G, pagerank, 'pagerank')
 
-# g4.from_nx(G)
-i = 0
+# Conductance
+# Find the community with the lowest conductance
+min_cond = float('inf')
+min_comm = None
 
+for c in comm:
+    # Calculate the conductance of the community
+    cond = cuts.conductance(G, c)
+    # Check if this community has lower conductance
+    if cond < min_cond:
+        min_cond = cond
+        min_comm = c
+
+ground_truth = {}
 for node in G.nodes(data=True):
-    g4.add_node(i, label=str(node[0]), size=node[1]["size"], betweenness_centrality=node[1]["betweenness_centrality"],
-                eigenvector_centrality=node[1]["eigenvector_centrality"],
-                closeness_centrality=node[1]["closeness_centrality"],
-                community=node[1]["community"], pagerank=node[1]["pagerank"])
-    i = i+1
-g4.from_nx(G)
-g4.show_buttons(filter_=['physics'])
-g4.write_html('karate.html', False, False)
+    ground_truth[node[0]] = node[1]["club"] # change this when data is changed
 
-# TODO: text to view the score of task community detection evaluations
-# TODO: create table to view each score of task page rank
-# TODO: ask shery is colab ok ?
+# Calculate the NMI score between the two clusterings
+nmi_score = normalized_mutual_info_score(
+    list(ground_truth.values()), list(communities.values()))
+
+nodesHTML = []
+
+print("graph")
+i = 0
+for node in G.nodes(data=True):
+    nodesHTML.append(node)
+
+    bias = node[1]["degree"]/max_color * 255
+    red = hex(int(bias))[2:]
+    blue = hex(int(256-bias))[2:]
+
+    if len(red) == 1:
+        red = '0'+red
+    if len(blue) == 1:
+        blue = '0'+blue
+    node_color = '#'+red+'00'+blue
+
+    graph.add_node(node[0], label=str(node[0]), size=node[1]["size"],
+                    betweenness_centrality=node[1]["betweenness_centrality"],
+                    eigenvector_centrality=node[1]["eigenvector_centrality"],
+                    closeness_centrality=node[1]["closeness_centrality"],
+                    community=node[1]["community"], pagerank=node[1]["pagerank"], color=node_color)
+    i = i+1
+
+graph.from_nx(G)
+graph.show_buttons(filter_=['physics'])
+graph.write_html('karate.html', False, False)
